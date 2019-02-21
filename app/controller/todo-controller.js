20acb9e0f0e0d27be04ad544077c625905f72b0f
Kura <SriVarsha.Kura@thomsonreuters.com>
2019-02-21 13:20:53 +0530

    added all functionalities

--- app/controller/todo-controller.js
+++ app/controller/todo-controller.js
@@ -9,13 +9,16 @@ function ($scope) {
 
   $scope.vm.addTask = function addTask() {
     if($scope.vm.taskName) {
-      $scope.vm.tasks.push($scope.vm.taskName);
+      var task={};
+     task.name = $scope.vm.taskName;
+      task.cheched = false;
+      $scope.vm.tasks.push(task);
       $scope.vm.taskName = null;
     }
   };
-  $scope.vm.removeTask=function removeTask(taskName){
-    if(taskName) {
-      $scope.vm.tasks.splice(tasks.indexOf(taskName), 1);
+  $scope.vm.removeTask=function removeTask(task){
+    if(task != null) {
+      $scope.vm.tasks.splice($scope.vm.tasks.indexOf(task), 1);
     }
   }
 }]);
